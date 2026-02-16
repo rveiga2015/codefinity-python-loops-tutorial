9856f765122b5f1f3d9924837751e3d27751a348
rveiga2015 <>
2026-02-16 18:16:18 +0000

    Loop Control

---
+++ the_while_loop/loop_control/main.py
@@ -0,0 +1,15 @@
+# List of country names
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List to hold selected countries
+selected = []
+i = 0 
+for i < len(countries):
+    if countries[i][0] != "S":
+       i += 1
+       continue
+    else:
+
+
+# Testing
+print('First three countries starting with "S":', selected)
